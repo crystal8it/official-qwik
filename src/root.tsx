330a6fbace80d8ba29969adf5bff414d96e0c808
Wei Shu <josephheal12@gmail.com>
2023-08-02 11:09:16 +0800

    Update: try to insert gtm

--- src/root.tsx
+++ src/root.tsx
@@ -1,3 +1,4 @@
+// @ts-ignore
 import { component$ } from '@builder.io/qwik';
 import { QwikPartytown } from './components/partytown/partytown';
 import {
@@ -23,11 +24,27 @@ export default component$(() => {
           src="https://www.googletagmanager.com/gtag/js?id=G-C1G8SZW33Y"
         ></script>
 
+        <script type="text/partytown" dangerouslySetInnerHTML="
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-M32JDPX3');
+        "></script>
+
         <RouterHead />
       </head>
       <body lang="zh-TW">
         <RouterOutlet />
         <ServiceWorkerRegister />
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M32JDPX3"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
+        </noscript>
       </body>
     </QwikCityProvider>
   );
