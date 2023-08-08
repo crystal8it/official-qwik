4d58ebabf7e962343d339adc9bcd1eec93aca6ee
Wei Shu <josephheal12@gmail.com>
2023-08-08 15:50:04 +0800

    Update: added google adsense script tag

--- src/root.tsx
+++ src/root.tsx
@@ -24,13 +24,22 @@ export default component$(() => {
           src="https://www.googletagmanager.com/gtag/js?id=G-C1G8SZW33Y"
         ></script>
 
-        <script type="text/partytown" dangerouslySetInnerHTML="
+        <script
+          type="text/partytown"
+          dangerouslySetInnerHTML="
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-M32JDPX3');
-        "></script>
+        "
+        ></script>
+
+        <script
+          type="text/partytown"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7669818123382596"
+          crossOrigin="anonymous"
+        ></script>
 
         <RouterHead />
       </head>
